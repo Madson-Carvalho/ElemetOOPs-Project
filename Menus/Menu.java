ba4c5cb28092ed86ffd8d7ddb15ce1aa6e72f4e9
Madson-Carvalho <madsonraynnon@gmail.com>
2023-04-30 18:09:18 -0300

    Ajustando classe genérica de menu

--- Menus/Menu.java
+++ Menus/Menu.java
@@ -1,21 +1,12 @@
 package Menus;
 
 public abstract class Menu {
-  private int option;
 
-  public int getOption(){
-    return this.option;
-  }
+    public void exitProgram() {
+        System.out.println("Encerrando programa...");
+        System.exit(0);
+    }
 
-  public void setOption(int option) {
-    this.option = option;
-  }
-
-  public void exitProgram() {
-    System.out.println("Sair do programa...");
-    System.exit(0);
-  }
-
-  public abstract void displayMenu();
+    public abstract void displayMenu();
 
 }
