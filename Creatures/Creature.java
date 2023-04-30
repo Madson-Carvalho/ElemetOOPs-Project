c7867599c12983e3cbe0084887d2c9217ee5be6d
Madson-Carvalho <madsonraynnon@gmail.com>
2023-04-30 16:19:39 -0300

    Criando classe de criatura de terra e ajustando o método de ataque elemental.

--- Creatures/Creature.java
+++ Creatures/Creature.java
@@ -59,5 +59,5 @@ public abstract class Creature {
         return  (getPower() * getAttack()) / defense;
     }
 
-    public abstract int elementalAttack (int defense);
+    public abstract int elementalAttack (int defense, String creatureType);
 }
